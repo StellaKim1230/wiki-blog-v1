1d50c226963f4a358f2424ee90c72fd1c9bd6042
Stellakim1230 <stellakim1230@gmail.com>
2020-04-12 23:06:15 +0900

    react 이름 규칙 작성 준비.

--- docs/.vuepress/configs/sidebar.js
+++ docs/.vuepress/configs/sidebar.js
@@ -1,13 +1,12 @@
 const sidebarConfig = [
   ['/si-escape', 'SI 탈출'],
   ['/2019-memoirs', '2019년 회고록'],
-  // {
-  //   title: 'Webpack',
-  //   children: [
-  //     ['/Webpack/01.Concept-of-webpack', '1. 웹팩'],
-  //     ['/Webpack/02.Setting-webpack-in-react-project', '2. 웹팩 세팅'],
-  //   ]
-  // }
+  {
+    title: 'React',
+    children: [
+      ['/React/01.naming-rule', '1. 이름 규칙'],
+    ]
+  }
 ]
 
 module.exports = sidebarConfig
