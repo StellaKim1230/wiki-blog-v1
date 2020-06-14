baed92c5d5b8cd70b4d2805c4a9a53756d902df2
Stellakim1230 <stellakim1230@gmail.com>
2020-06-14 22:57:30 +0900

    Programming, BookReport 주석처리.

--- docs/.vuepress/configs/sidebar.js
+++ docs/.vuepress/configs/sidebar.js
@@ -1,16 +1,16 @@
 const sidebarConfig = [
   ['/si-escape', 'SI 탈출'],
   ['/2019-memoirs', '2019년 회고록'],
-  {
-    title: 'Programming',
-    children: [
-      ['/Programming/01.description-react-structure', 'React Structure'],
-    ],
-  },
-  {
-    title: 'Book reports',
-    children: [['/BookReport/소프트웨어장인', '소프트웨어장인']],
-  },
+  // {
+  //   title: 'Programming',
+  //   children: [
+  //     ['/Programming/01.description-react-structure', 'React Structure'],
+  //   ],
+  // },
+  // {
+  //   title: 'Book reports',
+  //   children: [['/BookReport/소프트웨어장인', '소프트웨어장인']],
+  // },
 ]
 
 module.exports = sidebarConfig
