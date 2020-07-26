e74b9d749f70570e32f8ecec8e68b58f256c7c0c
Stellakim1230 <stellakim1230@gmail.com>
2020-07-26 19:47:48 +0900

    Add Disqus

--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -1,28 +1,30 @@
 <template>
   <ParentLayout>
-    <Disqus slot="page-bottom" v-if="this.$frontmatter.disqus" class="content" />
+    <template v-if="!this.$frontmatter.disableDisqus" #page-bottom>
+      <Disqus slot="page-bottom" class="content" />
+    </template>
   </ParentLayout>
 </template>
 
 <script>
-import ParentLayout from "@parent-theme/layouts/Layout";
-import Disqus from "../components/Disqus";
+import ParentLayout from '@parent-theme/layouts/Layout'
+import Disqus from '../components/Disqus'
 
 export default {
   components: {
     ParentLayout,
     Disqus,
   },
-  // watch: {
-  //   $route(to, from) {
-  //     if (to.path !== from.path) {
-  //       if (typeof window !== "undefined" && window.DISQUS) {
-  //         window.DISQUS.reset({ reload: true });
-  //       }
-  //     }
-  //   },
-  // },
-};
+  watch: {
+    $route(to, from) {
+      if (to.path !== from.path) {
+        if (typeof window !== 'undefined' && window.DISQUS) {
+          window.DISQUS.reset({ reload: true })
+        }
+      }
+    },
+  },
+}
 </script>
 
 <style lang="css" scoped>
