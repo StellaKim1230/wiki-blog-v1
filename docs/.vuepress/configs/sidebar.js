03f9410ca8e225b050e9f2b071e8f822739f9b48
Stellakim1230 <stellakim1230@gmail.com>
2020-12-28 17:34:32 +0900

    2020년 회고록 작성.

--- docs/.vuepress/configs/sidebar.js
+++ docs/.vuepress/configs/sidebar.js
@@ -1,6 +1,12 @@
 const sidebarConfig = [
   ['/si-escape', 'SI 탈출 스토리'],
-  ['/2019-memoirs', '2019년 회고록'],
+  {
+    title: '회고록',
+    children: [
+      ['/2019-memoris', '2019년 회고록'],
+      ['/2020-memoris', '2020년 회고록'],
+    ],
+  },
   {
     title: 'Programming',
     children: [
@@ -10,14 +16,15 @@ const sidebarConfig = [
   },
   {
     title: 'Algorithm',
-    children: [
-      ['/Algorithm/01.trie', 'Trie'],
-    ],
+    children: [['/Algorithm/01.trie', 'Trie']],
   },
   {
     title: 'Book reports',
     children: [
-      ['/BookReport/01.interface-without-interface', '인터페이스 없는 인터페이스'],
+      [
+        '/BookReport/01.interface-without-interface',
+        '인터페이스 없는 인터페이스',
+      ],
     ],
   },
 ]
--- docs/README.md
+++ docs/README.md
@@ -16,7 +16,7 @@ meta:
 
 SI에서 개발을 시작하였지만, 지금은 FE개발자로 일을 하고 있으며 항상 코드 품질을 높이기 위해 고민하고 노력합니다.
 
-- Jul 2020 ~ Sotfware engineer at Cobalt.
+- Jul 2020 ~ Frontend engineer at Cobalt.
 
   - [케이플](https://caple.ai/)
   - [케이플 디자인 시스템](https://design.caple.ai/)
