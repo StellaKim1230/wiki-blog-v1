46d7933b1c3ea7c5d7776786cc91de267494760b
Stellakim1230 <stellakim1230@gmail.com>
2020-12-28 17:34:32 +0900

    Ready 2020 memoris blog.

--- docs/.vuepress/configs/sidebar.js
+++ docs/.vuepress/configs/sidebar.js
@@ -1,6 +1,7 @@
 const sidebarConfig = [
   ['/si-escape', 'SI 탈출 스토리'],
-  ['/2019-memoirs', '2019년 회고록'],
+  ['/2019-memoris', '2019년 회고록'],
+  // ['/2020-memoris', '2020년 회고록'],
   {
     title: 'Programming',
     children: [
@@ -10,14 +11,15 @@ const sidebarConfig = [
   },
   {
     title: 'Algorithm',
-    children: [
-      ['/Algorithm/01.trie', 'Trie'],
-    ],
+    children: [['/Algorithm/01.trie', 'Trie']],
   },
   {
     title: 'Book reports',
     children: [
-      ['/BookReport/01.interface-without-interface', '인터페이스 없는 인터페이스'],
+      [
+        '/BookReport/01.interface-without-interface',
+        '인터페이스 없는 인터페이스',
+      ],
     ],
   },
 ]
---
+++ docs/2020-memoris.md
@@ -0,0 +1,22 @@
+---
+title: 2020년 회고록
+meta:
+  - name: description
+    content: 2020년 회고록
+  - property: og:title
+    content: 2020년 회고록
+  - property: og:description
+    content: 2020년 회고록, 프론트엔드 개발자 회고록, 회고, 프론트엔드 개발, 2020년
+---
+
+# GoodBye in 2020
+
+## 코로나
+
+## 7월 20일 새로운 시작
+
+## 재테크
+
+## 주식, 스톡옵션, 투자
+
+## 스터디
